036a32a2c3cecb01efaeb1a2a75d0286af6471cb
Timothy Vitkin <timothy.vitkin@gmail.com>
2020-04-25 16:25:42 -0400

    Go to home and animations

--- SampleApp/app/src/main/java/com/example/myapplication/facetime_calling_activity.java
+++ SampleApp/app/src/main/java/com/example/myapplication/facetime_calling_activity.java
@@ -1,64 +1,80 @@
 
 	 
 	/*
-	 *	This content is generated from the API File Info.
-	 *	(Alt+Shift+Ctrl+I).
-	 *
-	 *	@desc 		
-	 *	@file 		facetime_calling
-	 *	@date 		0
-	 *	@title 		Facetime_calling
-	 *	@author 	
-	 *	@keywords 	
-	 *	@generator 	Export Kit v1.2.9.xd
-	 *
-	 */
-	
-
-package com.example.myapplication;
-
-import android.app.Activity;
-import android.os.Bundle;
+     *	This content is generated from the API File Info.
+     *	(Alt+Shift+Ctrl+I).
+     *
+     *	@desc
+     *	@file 		facetime_calling
+     *	@date 		0
+     *	@title 		Facetime_calling
+     *	@author
+     *	@keywords
+     *	@generator 	Export Kit v1.2.9.xd
+     *
+     */
 
 
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+    package com.example.myapplication;
 
-public class facetime_calling_activity extends Activity {
+    import android.app.Activity;
+	import android.app.ActivityOptions;
+	import android.content.Intent;
+	import android.os.Build;
+	import android.os.Bundle;
 
-	
-	private View _bg__facetime_calling;
-	private View ellipse_1;
-	private ImageView _0;
-	private TextView dr__eustis;
-	private TextView calling_;
-	private ImageView polygon_1;
-	private TextView home;
-	private View ellipse_2;
-	private ImageView icon_awesome_phone_slash;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    import android.view.View;
+    import android.widget.ImageButton;
+    import android.widget.ImageView;
+    import android.widget.TextView;
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.facetime_calling);
+	import androidx.annotation.RequiresApi;
 
-		
-		_bg__facetime_calling = (View) findViewById(R.id._bg__facetime_calling);
-		ellipse_1 = (View) findViewById(R.id.ellipse_1);
-		_0 = (ImageView) findViewById(R.id._0);
-		dr__eustis = (TextView) findViewById(R.id.dr__eustis);
-		calling_ = (TextView) findViewById(R.id.calling_);
-		//polygon_1 = (ImageView) findViewById(R.id.polygon_1);
-		home = (TextView) findViewById(R.id.home);
-		ellipse_2 = (View) findViewById(R.id.ellipse_2);
-		icon_awesome_phone_slash = (ImageView) findViewById(R.id.icon_awesome_phone_slash);
-	
-		
-		//custom code goes here
-	
-	}
-}
+	public class facetime_calling_activity extends Activity {
+
+
+        private View _bg__facetime_calling;
+        private View ellipse_1;
+        private ImageView _0;
+        private TextView dr__eustis;
+        private TextView calling_;
+        private ImageView polygon_1;
+        private TextView home;
+        private View ellipse_2;
+        private ImageButton icon_awesome_phone_slash;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.facetime_calling);
+
+
+            _bg__facetime_calling = (View) findViewById(R.id._bg__facetime_calling);
+            ellipse_1 = (View) findViewById(R.id.ellipse_1);
+            _0 = (ImageView) findViewById(R.id._0);
+            dr__eustis = (TextView) findViewById(R.id.dr__eustis);
+            calling_ = (TextView) findViewById(R.id.calling_);
+            //polygon_1 = (ImageView) findViewById(R.id.polygon_1);
+            home = (TextView) findViewById(R.id.home);
+            ellipse_2 = (View) findViewById(R.id.ellipse_2);
+            icon_awesome_phone_slash = (ImageButton) findViewById(R.id.icon_awesome_phone_slash);
+
+            icon_awesome_phone_slash.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    goHome();
+                }
+            });
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+		public void goHome() {
+			Intent intent = new Intent(this, home_activity.class);
+			startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        }
+    }
 	
 	
