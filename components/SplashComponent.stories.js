f491564b4c963f32ff43711e97ea9eba47a81a66
WeustiS <willceustis@gmail.com>
2020-04-25 02:03:31 -0400

    Pushing app 101

---
+++ components/SplashComponent.stories.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Splash from "./SplashComponent";
+
+export const myProps = {
+  id: "1",
+  className: "mySampleClass",
+  sampleString: "Test Task",
+  sampleDate: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onActionOne: action("onActionOne"),
+  onActionTwo: action("onActionTwo")
+};
+
+storiesOf("Splash", module)
+  .add("default", () => <Splash {...myProps} {...actions} />)
+  .add("pinned", () => <Splash {...myProps} pinned={true} {...actions} />)
+  .add("archived", () => <Splash {...myProps} archived={true} {...actions} />);
---
+++ node_modules/@tensorflow/tfjs-react-native/dist/version.d.ts
@@ -0,0 +1,3 @@
+/** @license See the LICENSE file. */
+declare const version = "0.2.3";
+export { version };
